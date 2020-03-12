720f56f1b71fa76acd0f90f846d1b93e3fb8d368
Tomasz Zieba <tomasz.zieba@showpad.com>
2020-03-12 23:11:30 +0100

    💄 Build recipe list UI

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [];
 
   constructor() {
+    this.recipes.push(
+      new Recipe(
+        'Test Recipe',
+        'This is test recipe description',
+        'https://p0.pxfuel.com/preview/431/473/191/noodle-dish-chicken-farmer-cook.jpg')
+    );
+    this.recipes.push(
+      new Recipe(
+        'Test Recipe 2',
+        'This is test recipe 2 description',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnfFLUKS_sIeU0PngAkw6_lj98xOUQeYc03G1HLqz10G_tX_2XGw&s')
+    );
   }
 
   ngOnInit(): void {
